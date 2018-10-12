7902831f3140c8eb511d50712079efde8e8b896e
Bilal Tahir <you@example.com>
2018-10-13 03:55:01 +0500

    Static Files Check Commit

--- MobileApi/views.py
+++ MobileApi/views.py
@@ -7,7 +7,8 @@ from rest_framework.views import APIView
 from .serializers import RecommendedItemsSerializer
 #from mysite import settings as stcd
 import smtplib
-
+import turicreate as tc
+import settings as setting
 # Create your views here.
 
 
@@ -17,6 +18,8 @@ class ItemsUserApi(APIView):
         x.ItemID="111"
         x.ItemName="222"
         x.TimeStamp="222"
+        model=tc.load_model("my_model.model")
+        model
         serializer=RecommendedItemsSerializer(x)
         return Response(serializer.data)
     def post(self):
