e1e181fd2ac631ffca23f7d6c4934b2f03c41ba3
Bilal Tahir <you@example.com>
2018-10-13 20:48:11 +0500

    he

--- RecSystem/settings.py
+++ RecSystem/settings.py
@@ -127,8 +127,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
